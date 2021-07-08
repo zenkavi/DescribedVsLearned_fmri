165202f7a914919769af51d09525fce890754ec0
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-07-08 12:44:54 -0700

    debugs and trying to speed up compute node configuration

--- preproc/00_aws/test-setup-env.sh
+++ preproc/00_aws/test-setup-env.sh
@@ -11,25 +11,27 @@ if [[ "${cfn_node_type}" == "MasterServer" ]]
 then
   mkdir /shared/.out
   mkdir /shared/.err
+
+  export DATA_PATH=/shared/raw_fmri_data
+  export CODE_PATH=/shared/01_bidsify
+  export OUT_PATH=/shared/bids_nifti_wface
+
+  aws s3 sync s3://described-vs-experienced/01_bidsify $CODE_PATH
+
+  if [[ ! -e $OUT_PATH ]]; then
+    mkdir $OUT_PATH
+    aws s3 sync s3://described-vs-experienced/bids_nifti_wface $OUT_PATH
+  fi
+
+  if [[ ! -e $DATA_PATH ]]; then
+    mkdir $DATA_PATH
+  fi
+
+  chown -R ec2-user: $DATA_PATH
+  chown -R ec2-user: $CODE_PATH
+  chown -R ec2-user: $OUT_PATH
+  chown -R ec2-user: /shared/.out
+  chown -R ec2-user: /shared/.err
+
+  alias squeue='squeue -o "%.18i %.9P %.18j %.8u %.2t %.10M %.6D %R"'
 fi
-
-export DATA_PATH=/shared/raw_fmri_data
-export CODE_PATH=/shared/01_bidsify
-export OUT_PATH=/shared/bids_nifti_wface
-
-aws s3 sync s3://described-vs-experienced/01_bidsify $CODE_PATH
-
-if [[ ! -e $OUT_PATH ]]; then
-  mkdir $OUT_PATH
-  aws s3 sync s3://described-vs-experienced/bids_nifti_wface $OUT_PATH
-fi
-
-if [[ ! -e $DATA_PATH ]]; then
-  mkdir $DATA_PATH
-fi
-
-chown -R ec2-user: $DATA_PATH
-chown -R ec2-user: $CODE_PATH
-chown -R ec2-user: $OUT_PATH
-
-alias squeue='squeue -o "%.18i %.9P %.18j %.8u %.2t %.10M %.6D %R"'
