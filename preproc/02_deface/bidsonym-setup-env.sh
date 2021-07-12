44769c34b56643e8619509ebd85acf20789c17cd
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-07-12 16:11:05 -0700

    corrected data sync

--- preproc/02_deface/bidsonym-setup-env.sh
+++ preproc/02_deface/bidsonym-setup-env.sh
@@ -19,12 +19,12 @@ then
 
   if [[ ! -e $DATA_PATH ]]; then
     mkdir $DATA_PATH
-    aws s3 sync s3://described-vs-experienced/bids_nifti_wface/dataset_description.json $DATA_PATH/dataset_description.json
-    aws s3 sync s3://described-vs-experienced/bids_nifti_wface/participants.json $DATA_PATH/participants.json
-    aws s3 sync s3://described-vs-experienced/bids_nifti_wface/participants.tsv $DATA_PATH/participants.tsv
-    aws s3 sync s3://described-vs-experienced/bids_nifti_wface/task-bundles_bold.json $DATA_PATH/task-bundles_bold.json
-    aws s3 sync s3://described-vs-experienced/bids_nifti_wface/CHANGES $DATA_PATH/CHANGES
-    aws s3 sync s3://described-vs-experienced/bids_nifti_wface/README $DATA_PATH/README
+    aws s3 cp s3://described-vs-experienced/bids_nifti_wface/dataset_description.json $DATA_PATH/dataset_description.json
+    aws s3 cp s3://described-vs-experienced/bids_nifti_wface/participants.json $DATA_PATH/participants.json
+    aws s3 cp s3://described-vs-experienced/bids_nifti_wface/participants.tsv $DATA_PATH/participants.tsv
+    aws s3 cp s3://described-vs-experienced/bids_nifti_wface/task-bundles_bold.json $DATA_PATH/task-bundles_bold.json
+    aws s3 cp s3://described-vs-experienced/bids_nifti_wface/CHANGES $DATA_PATH/CHANGES
+    aws s3 cp s3://described-vs-experienced/bids_nifti_wface/README $DATA_PATH/README
   fi
 
   chown -R ec2-user: $DATA_PATH
