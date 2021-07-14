8c7ea77f43f70612d8e1a7571e6c6b00fef4e275
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-07-14 10:05:47 -0700

    cont working on physio preproc

--- preproc/01_bidsify/physio_preproc.py
+++ preproc/01_bidsify/physio_preproc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import datetime
+import json
 import numpy as np
 import os
 from pathlib import Path
@@ -48,24 +49,24 @@ for i, cur_sub in enumerate(subnums):
         # Within the vector of voltage values are “trigger” events from the scanner. These are entered as 5000 (for trigger on) and 5003 (for trigger off). These values need to be stripped out of the vector. There will occasionally be extra values at the end of the voltage vector as final values in the buffer will be written to the file after the logging is stopped. This can result in the vector length being slightly longer than would be predicted from the log start and stop times described below.
         ts = np.array([v for v in values if v < 5000])
 
-    #LogStartMDHTime
-    # Most relevant are the Log Start and Stop times for the MDH and MPCU. These log times indicate the time at which pulse-ox recording was started and stopped. That is, the first pulse-ox value in the first line of the data file was acquired at the LogStart time. The MPCU values provide time-stamps derived from the clock within the PMU recording system. The MDH values are time-stamps derived from the clock in the scanner, which is the same clock used to provide time-stamps for DICOM images. The MDH values are therefore preferred for synchronization of the DICOM images with the physiologic recording log.
-    log_start_time = lines[-5]
-    log_start_time = int(log_start_time.split(" ")[2])
-    hour = int(log_start_time / 1000 / 60 / 60)
-    min = int(log_start_time / 1000 / 60) - hour * 60
-    sec = int(log_start_time / 1000) - (hour * 60 * 60 + min * 60)
-    msec = log_start_time - (hour * 1000 * 60 * 60 + min * 1000 * 60 + sec * 1000)
-    log_start_time = datetime.time(hour, min, sec, msec * 1000)
-    #sub-01_task-bundles_run-1_bold.json -> time.AcquisitionTime[0]
-    fn = os.path.join(DATA_PATH, 'bids_nifti_wface/sub-%s/func/sub-%s_task-bundles_run-%s_bold.json'%(cur_sub, cur_sub, cur_run))
-    f = open(fn)
-    run_sidecar = json.load(f)
-    f.close()
-    scan_start_time = run_sidecar["AcquisitionTime"]
-    datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-    datetime.strptime(scan_start_time, '%H:%M:%S.%f')
-    rel_start_time = (log_start_time - scan_start_time)/1000
+        #LogStartMDHTime
+        # Most relevant are the Log Start and Stop times for the MDH and MPCU. These log times indicate the time at which pulse-ox recording was started and stopped. That is, the first pulse-ox value in the first line of the data file was acquired at the LogStart time. The MPCU values provide time-stamps derived from the clock within the PMU recording system. The MDH values are time-stamps derived from the clock in the scanner, which is the same clock used to provide time-stamps for DICOM images. The MDH values are therefore preferred for synchronization of the DICOM images with the physiologic recording log.
+        log_start_time = lines[-5]
+        log_start_time = int(log_start_time.split(" ")[2])
+
+        #sub-<cur_sub>_task-bundles_run-<cur_run>_bold.json -> time.AcquisitionTime[0]
+        fn = os.path.join(DATA_PATH, 'bids_nifti_wface/sub-%s/func/sub-%s_task-bundles_run-%s_bold.json'%(cur_sub, cur_sub, cur_run))
+        f = open(fn)
+        run_sidecar = json.load(f)
+        f.close()
+        scan_start_time = run_sidecar["AcquisitionTime"]
+        tmp = scan_start_time.split(":")
+        hour = int(tmp[0]) * 60 * 60 * 1000
+        min = int(tmp[1]) * 60 * 1000
+        sec = int(tmp[2].split(".")[0]) * 1000
+        msec = int(tmp[2].split(".")[1]) / 1000
+        scan_start_time = hour + min + sec + msec
+        rel_start_time = (log_start_time - scan_start_time)/1000
 
 #OUTPUTS
 
