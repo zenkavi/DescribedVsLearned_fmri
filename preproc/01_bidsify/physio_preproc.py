0cbb5d5d32a00231afe6a62d603b9d5c10960879
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-07-14 11:52:38 -0700

    cont working on physio preproc

--- preproc/01_bidsify/physio_preproc.py
+++ preproc/01_bidsify/physio_preproc.py
@@ -11,7 +11,7 @@ subnums = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12
 
 runnum_vals = ['1', '2', '3', '4', '5']
 
-log_type_dict = {'resp': 'breating', 'puls': 'cardiac', 'ext': 'trigger'}
+log_type_dict = {'resp': 'breathing', 'puls': 'cardiac', 'ext': 'trigger'}
 
 for i, cur_sub in enumerate(subnums):
 
@@ -32,28 +32,38 @@ for i, cur_sub in enumerate(subnums):
 
     for j, cur_file in enumerate(file_names):
 
+        # Get run number
         cur_run_key = cur_file.split("_")[3]
         cur_run = runnum_dict[cur_run_key]
 
+        # Get log type
+        cur_file_ext = cur_file.split(".")[1]
+        log_type = log_type_dict[cur_file_ext]
+
+        # Get time series of log
         content = Path(os.path.join(files_path, cur_file)).read_text()
-
         lines = content.splitlines()
-
         line = lines.pop(0)
-
         if 'ext' in cur_file:
             values = [int(v) for v in line.split(" ")[25:-1]]
+            num_dig = '%1d'
         else:
             values = [int(v) for v in line.split(" ")[20:-1]]
-
+            num_dig = '%4d'
         # Within the vector of voltage values are “trigger” events from the scanner. These are entered as 5000 (for trigger on) and 5003 (for trigger off). These values need to be stripped out of the vector. There will occasionally be extra values at the end of the voltage vector as final values in the buffer will be written to the file after the logging is stopped. This can result in the vector length being slightly longer than would be predicted from the log start and stop times described below.
         ts = np.array([v for v in values if v < 5000])
 
-        #LogStartMDHTime
+        # Write output
+        fn = os.path.join(DATA_PATH, 'bids_nifti_wface/sub-%s/func/sub-%s_task-bundles_run-%s_recording-%s_physio.tsv'%(cur_sub, cur_sub, cur_run, log_type))
+        np.savetxt(fn, ts, delimiter='\t', fmt=num_dig)
+
+        # Get log start time
+        # LogStartMDHTime
         # Most relevant are the Log Start and Stop times for the MDH and MPCU. These log times indicate the time at which pulse-ox recording was started and stopped. That is, the first pulse-ox value in the first line of the data file was acquired at the LogStart time. The MPCU values provide time-stamps derived from the clock within the PMU recording system. The MDH values are time-stamps derived from the clock in the scanner, which is the same clock used to provide time-stamps for DICOM images. The MDH values are therefore preferred for synchronization of the DICOM images with the physiologic recording log.
         log_start_time = lines[-5]
         log_start_time = int(log_start_time.split(" ")[2])
 
+        # Get scan start time
         #sub-<cur_sub>_task-bundles_run-<cur_run>_bold.json -> time.AcquisitionTime[0]
         fn = os.path.join(DATA_PATH, 'bids_nifti_wface/sub-%s/func/sub-%s_task-bundles_run-%s_bold.json'%(cur_sub, cur_sub, cur_run))
         f = open(fn)
@@ -66,7 +76,12 @@ for i, cur_sub in enumerate(subnums):
         sec = int(tmp[2].split(".")[0]) * 1000
         msec = int(tmp[2].split(".")[1]) / 1000
         scan_start_time = hour + min + sec + msec
+
+        # Computer relative start time for sidecars
         rel_start_time = (log_start_time - scan_start_time)/1000
+
+        # Write sidecar
+        ...
 
 #OUTPUTS
 
