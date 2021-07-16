2076ccdc2936b8f27235655f932026c47ce12384
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-07-16 14:09:09 -0700

    changing sbatch options to allocate one machine per job

--- preproc/00_aws/push_cluster_prereqs.sh
+++ preproc/00_aws/push_cluster_prereqs.sh
@@ -1,5 +1,4 @@
 #! /bin/zsh
 
 export STUDY_DIR=/Users/zeynepenkavi/Documents/RangelLab/DescribedVsLearned_fmri/preproc
-docker run --rm -it -v ~/.aws:/root/.aws -v $STUDY_DIR/00_aws:/home amazon/aws-cli s3 cp /home/test-setup-env.sh s3://described-vs-experienced/test-setup-env.sh
-docker run --rm -it -v ~/.aws:/root/.aws -v $STUDY_DIR:/home amazon/aws-cli s3 sync /home/01_bidsify s3://described-vs-experienced/01_bidsify --exclude ".DS_Store"
+docker run --rm -it -v ~/.aws:/root/.aws -v $STUDY_DIR:/base amazon/aws-cli s3 sync /base/01_bidsify s3://described-vs-experienced/01_bidsify --exclude ".DS_Store"
