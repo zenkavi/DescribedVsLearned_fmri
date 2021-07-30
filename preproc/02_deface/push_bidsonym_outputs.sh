9b060ffcc5b01a33f7538addbd5b85384e052b18
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-07-30 16:53:14 -0700

    more debugging

--- preproc/02_deface/push_bidsonym_outputs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aws s3 sync $DATA_PATH/sub-$1 s3://described-vs-experienced/bids_nifti_wface/sub-$1
-aws s3 sync $DATA_PATH/sourcedata/bidsonym s3://described-vs-experienced/bids_nifti_wface/sourcedata/bidsonym
-sudo chown -R ec2-user: $DATA_PATH/sub-$1
-sudo chown -R ec2-user: $DATA_PATH/sourcedata/bidsonym
-rm -rf $DATA_PATH/sub-$1
-rm -rf $DATA_PATH/sourcedata/bidsonym/sub-$1
