9b2053adde7184ac7645f8a382f1ab2624b5bbee
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-02 16:44:55 -0700

    cont working on level 1 analyses

--- analysis/01_level1/level1.py
+++ analysis/01_level1/level1.py
@@ -8,7 +8,8 @@ parser = ArgumentParser()
 parser.add_argument("-s", "--subnum", help="subject number")
 args = parser.parse_args()
 subnum = args.subnum
-data_loc = os.environ['DATA_PATH']
-code_path = os.environ['SERVER_SCRIPTS']
+data_path = os.environ['DATA_PATH']
+code_path = os.environ['CODE_PATH']
+out_path = os.environ['OUT_PATH']
 
-run_level1(subnum = subnum, out_path = "%s/derivatives/nilearn/level_1/sub-%s"%(data_loc,subnum), pe=True, pe_path='...'%(...), beta=False, ev=False)
+run_level1(subnum = subnum, out_path = "%s/sub-%s"%(out_path,subnum), pe=True, pe_path='...'%(...), beta=False, ev=False)
