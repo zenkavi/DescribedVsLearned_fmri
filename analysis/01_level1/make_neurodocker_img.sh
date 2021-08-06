45ac4b90020106bc80e5c6a13aba7cd783cc2d18
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-06 08:52:04 -0700

    adding neurodocker image maker

---
+++ analysis/01_level1/make_neurodocker_img.sh
@@ -0,0 +1,12 @@
+docker run --rm repronim/neurodocker:0.7.0 generate docker \
+  --pkg-manager apt \
+  --base debian:buster-slim \
+  --fsl version=6.0.3 \
+  --miniconda \
+       version=4.10.3 \
+       create_env=neuro \
+       pip_install="setuptools Cython nibabel nilearn nipype numpy pandas scipy" \
+       activate=true \
+> fsl603.Dockerfile
+
+docker build --tag fsl:6.0.3 --file fsl603.Dockerfile .
