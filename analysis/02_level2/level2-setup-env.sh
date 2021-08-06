1752f635c5745fbbf3858cd16cbee9d358ece05c
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-06 09:03:41 -0700

    refactoring and adding level specific setup files

--- analysis/02_level2/level2-setup-env.sh
+++ analysis/02_level2/level2-setup-env.sh
@@ -6,17 +6,15 @@ then
   mkdir /shared/.out
   mkdir /shared/.err
 
-  export DATA_PATH=/shared/bids_nifti_wface
-  export CODE_PATH=/shared/code/analysis/01_level1
-  export BEHAVIOR_PATH=/shared/behavioral_data/
-  export OUT_PATH=/shared/bids_nifti_wface/derivatives/nilearn/glm/level1
+  export DATA_PATH=/shared/bids_nifti_wface/derivatives/nilearn/glm/level1
+  export OUT_PATH=/shared/bids_nifti_wface/derivatives/nilearn/glm/level2
+  export CODE_PATH=/shared/code/analysis/02_level2
 
   if [[ ! -e $DATA_PATH ]]; then
     mkdir $DATA_PATH
   fi
 
-  aws s3 sync s3://described-vs-experienced/code/analysis/01_level1 $CODE_PATH
-  aws s3 cp s3://described-vs-experienced/behavioral_data/all_trials.csv $BEHAVIOR_PATH
+  aws s3 sync s3://described-vs-experienced/code/analysis/02_level2 $CODE_PATH
 
   if [[ ! -e $OUT_PATH ]]; then
     mkdir $OUT_PATH
@@ -27,7 +25,7 @@ then
   chown -R ec2-user: $OUT_PATH
   chown -R ec2-user: /shared/.out
   chown -R ec2-user: /shared/.err
-  chmod +x $CODE_PATH/level1.py
+  chmod +x $CODE_PATH/level2.py
 
   echo "alias squeue='squeue -o \"%.18i %.9P %.18j %.8u %.2t %.10M %.6D %R\"'">> /home/ec2-user/.bash_profile
 fi
