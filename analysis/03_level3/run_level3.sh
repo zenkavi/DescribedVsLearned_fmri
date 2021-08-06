5a858531724b18054677dbd86014dc32645cd575
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-06 12:47:15 -0700

    debug docker image path

--- analysis/03_level3/run_level3.sh
+++ analysis/03_level3/run_level3.sh
@@ -7,7 +7,7 @@ do
     do
       for regname in cross crossRt fractalProb fractalProbParam stim stimRt valDiff choiceLeft conflict noconflict reward rewardParam rpe
       do
-        sed -e "s/{MODELNUM}/$modelnum/g" "s/{SIGN}/$sign/g" "s/{TFCE}/$tfce/g" "s/{REGNAME}/$regname/g" run_level3.batch | sbatch
+        sed -e "s/{MODELNUM}/$modelnum/g" -e "s/{SIGN}/$sign/g" -e "s/{TFCE}/$tfce/g" -e "s/{REGNAME}/$regname/g" run_level3.batch | sbatch
       done
     done
   done
