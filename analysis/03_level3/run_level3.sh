b7a4cfdabfd5f449abd787b6a215fa3f99f366c0
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-06 11:33:50 -0700

    prepping job files for level 3

--- analysis/03_level3/run_level3.sh
+++ analysis/03_level3/run_level3.sh
@@ -1,8 +1,11 @@
 set -e
 for modelnum in model1
 do
-  for regname in cross crossRt fractalProb fractalProbParam stim stimRt valDiff choiceLeft conflict noconflict reward rewardParam rpe
+  for tfce in True False
   do
-    sed -e "s/{MODELNUM}/$modelnum/g" "s/{REGNAME}/$regname/g" run_level3.batch | sbatch
+    for regname in cross crossRt fractalProb fractalProbParam stim stimRt valDiff choiceLeft conflict noconflict reward rewardParam rpe
+    do
+      sed -e "s/{MODELNUM}/$modelnum/g" "s/{TFCE}/$tfce/g" "s/{REGNAME}/$regname/g" run_level3.batch | sbatch
+    done
   done
 done
