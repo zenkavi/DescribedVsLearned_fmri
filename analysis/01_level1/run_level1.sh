aad968e4913de597d165cf8d8035b4cb0f459071
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-12 09:13:59 -0700

    debug job submission

--- analysis/01_level1/run_level1.sh
+++ analysis/01_level1/run_level1.sh
@@ -3,6 +3,6 @@ for subnum in 04 05
 do
   for reg_rt in 0 1
   do
-    sed -e "s/{SUBNUM}/$subnum/g" "s/{REG_RT}/$reg_rt/g" run_level1.batch | sbatch
+    sed -e "s/{SUBNUM}/$subnum/g" -e "s/{REG_RT}/$reg_rt/g" run_level1.batch | sbatch
   done
 done
