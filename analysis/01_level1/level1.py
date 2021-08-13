45bb082dc7b47fb4cb1f2e6ae189917b8ac58bf7
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-13 11:39:22 -0700

    debug rt reg

--- analysis/01_level1/level1.py
+++ analysis/01_level1/level1.py
@@ -10,7 +10,7 @@ parser.add_argument("-s", "--subnum", help="subject number")
 parser.add_argument("--reg_rt", help="regress rt", default=1)
 args = parser.parse_args()
 subnum = args.subnum
-reg_rt = args.reg_rt
+reg_rt = int(args.reg_rt)
 data_path = os.environ['DATA_PATH']
 behavior_path = os.environ['BEHAVIOR_PATH']
 out_path = os.environ['OUT_PATH']
