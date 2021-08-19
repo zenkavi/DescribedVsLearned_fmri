06597cbd2f4737ab0ea25e175544c02a553b2545
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-19 14:53:35 -0700

    add script to get average cormat for design matrices

---
+++ analysis/03_level3/get_mean_desmat_cor.py
@@ -0,0 +1,41 @@
+#! /opt/miniconda-4.10.3/bin/python
+
+from argparse import ArgumentParser
+import glob
+import os
+import pandas as pd
+
+parser = ArgumentParser()
+parser.add_argument("--mnum", help="model number")
+parser.add_argument("--reg_rt", help="regress rt")
+parser.add_argument("--l1_path")
+parser.add_argument("--l3_path")
+
+args = parser.parse_args()
+mnum = args.mnum
+reg_rt = args.reg_rt
+l1_path = args.l1_path
+l3_path = args.l3_path
+
+def get_mean_desmat_cor(mnum, reg_rt, l1_path):
+
+    des_mats = glob.glob(os.path.join(l1_path, 'model%s_reg-rt%s/**/*design_matrix*'%(mnum, reg_rt)))
+
+    beh_regs = pd.read_csv(des_mats[0]).columns
+    to_filter = ['trans', 'rot', 'drift', 'framewise', 'scrub', 'constant', 'dvars']
+    beh_regs = [x for x in beh_regs if all(y not in x for y in to_filter)]
+
+    cor_mats = []
+    for i, cur_des_mat in enumerate(des_mats):
+        cur_des_mat = pd.read_csv(cur_des_mat)
+        cor_mats.append(cur_des_mat[beh_regs].corr())
+
+    df_concat = pd.concat(cor_mats)
+    by_row_index = df_concat.groupby(df_concat.index)
+    df_means = by_row_index.mean()
+
+    return df_means
+
+mean_cor_df = get_mean_desmat_cor(mnum, reg_rt, l1_path)
+
+mean_cor_df.to_csv(os.path.join(l3_path, 'model%s_reg-rt%s_mean_desmat_cor.csv'%(mnum, reg_rt)), index=False)
---
+++ analysis/03_level3/get_mean_desmat_cor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export L1_PATH=/shared/bids_nifti_wface/derivatives/nilearn/glm/level1/
+export L3_PATH=/shared/bids_nifti_wface/derivatives/nilearn/glm/level3/
+export CODE_PATH=/shared/code/analysis/03_level3
+
+for modelnum in model1
+do
+  for reg_rt in 0 1
+  do
+    aws s3 sync s3://described-vs-experienced/bids_nifti_wface/derivatives/nilearn/glm/level1/$modelnum_reg-rt$reg_rt $DATA_PATH/$modelnum_reg-rt$reg_rt
+
+    docker run --rm -it \
+    -v $L1_PATH:/data -v $L3_PATH:/out -v $CODE_PATH:/code \
+    zenkavi/fsl:6.0.3 ./code/get_mean_desmat_cor.py --mnum $modelnum --reg_rt $reg_rt --l1_path /data --l3_path /out
+
+    aws s3 sync $L3_PATH/$modelnum_reg-rt$reg_rt s3://described-vs-experienced/bids_nifti_wface/derivatives/nilearn/glm/level3/$modelnum_reg-rt$reg_rt/
+  done
+done
