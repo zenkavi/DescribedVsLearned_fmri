406f15de581a9d6019cd8c1acfda48cbcfc3a5e4
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-08-30 17:36:23 -0700

    fix typo and new images

--- analysis/03_level3/run_level3/run_level3_rtregs.sh
+++ analysis/03_level3/run_level3/run_level3_rtregs.sh
@@ -1,7 +1,7 @@
 set -e
 for modelnum in model1 model1a model2 model3 model4 model5 model5a model6 model6a model7 model8 model9 model10 model10a
 do
-  for modelname in overall_mean
+  for modelname in overall-mean
   do
     for sign in pos neg
     do
