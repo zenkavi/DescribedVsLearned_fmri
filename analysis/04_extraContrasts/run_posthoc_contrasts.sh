6ed11e6671d27b07d05a0894362478e1ec608110
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-09-13 17:32:34 -0700

    debugging posthoc contrast pipeline

--- analysis/04_extraContrasts/run_posthoc_contrasts.sh
+++ analysis/04_extraContrasts/run_posthoc_contrasts.sh
@@ -1,5 +1,5 @@
 set -e
-for modelnum in model1
+for modelnum in model7a
 do
   for modelname in overall-mean
   do
@@ -11,7 +11,7 @@ do
         do
           for reg_rt in 0
           do
-            sed -e "s/{MODELNUM}/$modelnum/g" -e "s/{MODELNAME}/$modelname/g" -e "s/{SIGN}/$sign/g" -e "s/{TFCE}/$tfce/g" -e "s/{REGNAME}/$regname/g" -e "s/{REG_RT}/$reg_rt/g" run_posthoc_contrast.batch | sbatch
+            sed -e "s/{MODELNUM}/$modelnum/g" -e "s/{MODELNAME}/$modelname/g" -e "s/{SIGN}/$sign/g" -e "s/{TFCE}/$tfce/g" -e "s/{REGNAME}/$regname/g" -e "s/{REG_RT}/$reg_rt/g" run_posthoc_contrasts.batch | sbatch
           done
         done
       done
