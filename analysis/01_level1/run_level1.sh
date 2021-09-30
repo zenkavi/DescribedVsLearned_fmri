8287b5a60bb176ab7e2538e46dc5ab5d087ef6e5
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-09-30 14:27:44 -0700

    running other models again

--- analysis/01_level1/run_level1.sh
+++ analysis/01_level1/run_level1.sh
@@ -1,5 +1,5 @@
 set -e
-for modelnum in model3
+for modelnum in model4 model5 model6 model7
 do
   for subnum in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 22 23 24 25 27
   do
---
+++ analysis/02_level2/run_level2/run_level2_model7.sh
@@ -0,0 +1,23 @@
+set -e
+for modelnum in model7
+do
+  for modelname in overall-mean
+  do
+    for sign in pos neg
+    do
+      for tfce in 1
+      do
+        for regname in fractalProb_ev fractalProb_par stim_ev choiceShift_st valBundleSum_par valChosenMinusUnchosen_par reward_ev reward_par rpeLeftFractal_par
+        do
+          for reg_rt in 0
+          do
+            for package in fsl
+            do
+              sed -e "s/{MODELNUM}/$modelnum/g" -e "s/{MODELNAME}/$modelname/g" -e "s/{SIGN}/$sign/g" -e "s/{TFCE}/$tfce/g" -e "s/{REGNAME}/$regname/g" -e "s/{REG_RT}/$reg_rt/g" -e "s/{PACKAGE}/$package/g" run_level2.batch | sbatch
+            done
+          done
+        done
+      done
+    done
+  done
+done
